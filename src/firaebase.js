9df09f148cbb672b967e90d96931bfccc594209b
Ishita Keshawani <keshwaniishita054@gmail.com>
2021-05-16 17:15:22 +0530

    hosting on firabase

--- src/firaebase.js
+++ src/firaebase.js
@@ -2,12 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyC6TXgEU69czCKddOZ2DMkT98WWjm7XDkQ",
+    authDomain: "authentication-test-da382.firebaseapp.com",
+    projectId: "authentication-test-da382",
+    storageBucket: "authentication-test-da382.appspot.com",
+    messagingSenderId: "810667155836",
+    appId: "1:810667155836:web:0ee24c078c851ad6cbc4f9",
+    measurementId: "G-HL0V22ZHXW"
 })
 
 export const auth = app.auth();
